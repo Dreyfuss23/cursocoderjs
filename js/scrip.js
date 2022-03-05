2770435fe9252a6e7673d811b702310719360130
Juan <juannramos@outlook.es>
2022-03-05 02:37:11 -0300

    hellow

--- js/scrip.js
+++ js/scrip.js
@@ -1,22 +1,4 @@
-//debugger
-/* let entrada = prompt("Ingresa tu nombre");
-let mensaje1 = "hola"
-let mensaje = "bienvenido a mi web de javascript";
-let resultado =mensaje1 +" " +entrada +" "+ mensaje;
+let precio = (prompt("ingrese su valor"));
+let precioConIva = precio *1.65;
 
-alert(resultado); */
-
-let num1 , num2;
-
-num1 = Number(prompt("Ingrese tu primer numero a sumar")) ;
-
-num2 = Number (prompt ("ingrese tu segundo numero a sumar"));
-
-
-let resultado = num1 + num2;
-
-alert (resultado);
-
-
-//parseInt --> 50
-//parseFloat --> 50.6
+alert (precioConIva)
