e6d33d7b6e3101e4efa43040686778568bd17353
Juan <juannramos@outlook.es>
2022-03-05 02:43:22 -0300

    heldsa

--- js/scrip.js
+++ js/scrip.js
@@ -1,4 +1,4 @@
-let precio = (prompt("ingrese su valor"));
-let precioConIva = precio *1.65;
+//let precio = (prompt("ingrese su valor"));
+//let precioConIva = precio *1.65;
 
-alert (precioConIva)
+//alert (precioConIva)
