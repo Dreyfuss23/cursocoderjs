2be2a52a9b59016daa139b3da20bc4e05ac1d531
Juan <juannramos@outlook.es>
2022-03-08 00:49:32 -0300

    heasdkl

--- js/scrip.js
+++ js/scrip.js
@@ -1,4 +1,9 @@
 let precio = Number(prompt("ingrese el valor del juego, que va a comprar!"));
-let precioConIva = precio *1.65;
+let precioConIva = precio * 1.65;
 
-alert ("El precio de su juego es " + " " + precioConIva +"$");
+alert("El precio de su juego es " + " " + precioConIva + "$");
+
+while(precio != 5){
+    
+}
+
